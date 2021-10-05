bd7e4b56c04f160806759760abe48443f40b48f4
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-05 21:40:38 +0900

    update

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,12 +5,57 @@ import React, {
   useRef,
   useState
 } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
-import { State } from '../type'
+import { v1 as uuid } from 'uuid'
+import { Todo } from '../type'
 import './App.css'
 
+const todos: Todo[] = [
+  {
+    id: uuid(),
+    desc: 'Learn React',
+    isComplete: true
+  },
+  {
+    id: uuid(),
+    desc: 'Learn Redux',
+    isComplete: true
+  },
+  {
+    id: uuid(),
+    desc: 'Learn Redux-ToolKit',
+    isComplete: false
+  }
+]
+
+const selectedTodoId = todos[1].id
+const editedCount = 0
+
 const App = () => {
+  const [newTodoInput, setNewTodoInput] = useState<string>('')
+  const [editTodoInput, setEditTodoInput] = useState<string>('')
+  const [isEditMode, setIsEditMode] = useState<boolean>(false)
+  const editInput = useRef<HTMLInputElement>(null)
+
+  const selectedTodo =
+    (selectedTodoId && todos.find((todo) => todo.id === selectedTodoId)) || null
+
+  const handleNewInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setNewTodoInput(e.target.value)
+  }
+
+  const handleEditInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setEditTodoInput(e.target.value)
+  }
+
+  const handleSelectTodo = (todoId: string) => (): void => {}
+
+  const handleEdit = (): void => {
+    if (!selectedTodo) return
+
+    setEditTodoInput(selectedTodo.desc)
+    setIsEditMode(true)
+  }
+
   return (
     <div className="App">
       <div className="App__counter">Todos Updated Count:</div>
