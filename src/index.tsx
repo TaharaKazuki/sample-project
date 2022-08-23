96dde95a6b2ce6001bc4cbfc667653a682f2848a
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-23 23:52:25 +0900

    refresh update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-import App from './App'
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 
-ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
