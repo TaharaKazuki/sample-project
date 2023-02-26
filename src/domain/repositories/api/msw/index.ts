5ec00a5a1d67fdb339411a5310e4300d2ac25dee
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-02-26 10:14:15 +0900

    update

---
+++ src/domain/repositories/api/msw/index.ts
@@ -0,0 +1,16 @@
+import type { SetupWorkerApi } from 'msw';
+
+let worker: SetupWorkerApi | undefined;
+
+export const startMsw = async () => {
+  if (worker || process.env.NODE_ENV === 'test') return;
+  const { browserWorker } = await import('./browser');
+  worker = browserWorker;
+  browserWorker.start({ onUnhandledRequest: 'bypass' });
+};
+
+export const stopMsw = async () => {
+  if (!worker) return;
+  worker.stop();
+  worker = undefined;
+};
